d7d79cff9c3df841b70ec68dc9ddf87cc922ced0
Antoine Dabonneville <antoine.dabonneville@pm.me>
2022-04-14 13:58:00 +0200

    creation of new calendar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,10 @@
 import "./App.css";
-import CardShowList from "./components/CardShowList";
-import listEvent from "./components/event";
+import PageDate from "./pages/PageDate";
 
 function App() {
   return (
     <div className="App">
-      <h2> Composant CardShowcase2 : liste mappée à partir de tableau</h2>
-      <CardShowList events={listEvent.records} />
+      <PageDate />
     </div>
   );
 }
---
+++ frontend/src/pages/PageDate.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function PageDate() {
+  return (
+    <div>
+      <h1>Quand ?</h1>
+    </div>
+  );
+}
+
+export default PageDate;
