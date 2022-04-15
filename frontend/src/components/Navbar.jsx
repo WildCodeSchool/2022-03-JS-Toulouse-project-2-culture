2349ddf521e773af71fb9fd6efdb3eae3c3f06b6
pjamard <80617509+pjamard@users.noreply.github.com>
2022-04-15 09:56:14 +0200

    creation of the navbar with burger menu

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import Logo from "./Logo";
+import "./Navbar.css";
+
+function Navbar() {
+  const [showLinks, setShowLinks] = useState(false);
+
+  const handleShowLinks = () => {
+    setShowLinks(!showLinks);
+  };
+
+  return (
+    <nav className={`navbar ${showLinks ? "show-nav" : "hide-nav"}`}>
+      <div className="navbar_logo">
+        <Logo />
+      </div>
+      <ul className="navbar_links">
+        <li className="navbar_item">
+          <a href="/" className="navbar_link">
+            Accueil
+          </a>
+        </li>
+        <li className="navbar_item">
+          <a href="/" className="navbar_link">
+            Favoris
+          </a>
+        </li>
+        <li className="navbar_item">
+          <a href="/" className="navbar_link">
+            A propos
+          </a>
+        </li>
+      </ul>
+      <button type="button" className="navbar_burger" onClick={handleShowLinks}>
+        <span className="burger-bar" />
+      </button>
+    </nav>
+  );
+}
+
+export default Navbar;
