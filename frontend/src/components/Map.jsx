f5ff77829c6eb72a5467f83a17c44b76ebeed439
Xaboule <xaboule@gmail.com>
2022-04-22 10:23:27 +0200

    fix dependencies

--- frontend/src/components/Map.jsx
+++ frontend/src/components/Map.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import L from "leaflet";
-// import listEvent from "./event";
 import Green from "../assets/markers/Green.png";
 import Blue from "../assets/markers/Blue.png";
 import Yellow from "../assets/markers/Yellow.png";
