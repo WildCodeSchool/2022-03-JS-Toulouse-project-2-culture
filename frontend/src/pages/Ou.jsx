6236db9f30d310e3bf00b496f7219556e41de54d
pjamard <80617509+pjamard@users.noreply.github.com>
2022-04-28 13:24:13 +0200

    fix some bugs

--- frontend/src/pages/Ou.jsx
+++ frontend/src/pages/Ou.jsx
@@ -6,14 +6,13 @@ import listEvent from "../components/event";
 import "../components/Map.css";
 
 function Ou(props) {
-  const { /* events, */ selectedPlace, setSelectedPlace } = props;
+  const { selectedPlace, setSelectedPlace } = props;
   return (
     <div className="AskPlaceContainer">
       <h3>Où ?</h3>
       <Searchbar
         selectedPlace={selectedPlace}
         setSelectedPlace={setSelectedPlace}
-        // eventsSearch={events}
       />
       <Map events={listEvent.records} />
     </div>
