49fa7dc2f56aa96e894897cc8ad1d463dfdcf59b
Antoine Dabonneville <antoine.dabonneville@pm.me>
2022-04-28 23:58:50 +0200

    upgrade css Nav and pages margin top

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -41,7 +41,7 @@ function App() {
   };
   return (
     <div className="App">
-      {/* <Navbar /> */}
+      <Navbar />
       <Router>
         <ul>
           <li>
--- frontend/src/components/functions.jsx
+++ frontend/src/components/functions.jsx
@@ -3,17 +3,6 @@ export function dateJJMMConverter(stringDate) {
     month: "numeric",
     day: "numeric",
   });
-
-  // const dayNumber =
-  //   new Date(stringDate).getUTCDate() < 10
-  //     ? `0${new Date(stringDate).getUTCDate()}`
-  //     : new Date(stringDate).getUTCDate();
-  // const monthNumber =
-  //   new Date(stringDate).getUTCMonth() < 10
-  //     ? `0${new Date(stringDate).getUTCMonth()}`
-  //     : new Date(stringDate).getUTCMonth();
-
-  // return `${dayNumber}/${monthNumber}`;
 }
 export function filterByDate(array, date) {
   return array.filter(
