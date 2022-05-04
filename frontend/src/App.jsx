d5c20e5ac3c5fc10cba43e3547766817dc300a8f
Antoine Dabonneville <antoine.dabonneville@pm.me>
2022-05-04 15:28:45 +0200

    move all steps in a parcours components including button next/prev

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,39 +4,28 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import "./Background.scss";
 import "./App.css";
 import axios from "axios";
-import Quand from "./pages/Quand";
+// import Quand from "./pages/Quand";
 import Ou from "./pages/Ou";
 import Favoris from "./pages/Favoris";
 import Apropos from "./pages/Apropos";
 import Navbar from "./components/Navbar";
-import CardShowResults from "./components/CardShowResults";
-import Accueil from "./pages/Accueil";
+// import CardShowResults from "./components/CardShowResults";
+// import Accueil from "./pages/Accueil";
 import Detailspretext from "./components/Detailspretexte";
-import { filterByDate, filterByLocation } from "./components/functions";
-import BtnNav from "./components/BtnNav";
+import { filterByDate } from "./components/functions";
+// import BtnNav from "./components/BtnNav";
+import Parcours from "./components/parcours";
 
 function App() {
   const [eventArrayFromAPI, setEventArrayfromAPI] = useState([]);
   const [selectedDate, setSelectedDate] = useState(new Date());
   const [selectedPlace, setSelectedPlace] = useState("");
 
-  const [step, setStep] = useState(0);
   useEffect(() => {
     const url =
       "https://data.laregion.fr/api/records/1.0/search/?dataset=agendas-participatif-des-sorties-en-occitanie&rows=400";
     axios.get(url).then((res) => setEventArrayfromAPI(res.data.records));
   }, []);
-
-  const handleSubmitNext = () => {
-    if (step < 4) {
-      setStep(step + 1);
-    }
-  };
-  const handleSubmitPrev = () => {
-    if (step > 0) {
-      setStep(step - 1);
-    }
-  };
 
   return (
     <div className="App">
@@ -69,29 +58,13 @@ function App() {
           <Route
             path="/"
             element={
-              step === 0 ? (
-                <Accueil />
-              ) : step === 1 ? (
-                <Quand
-                  selectedDate={selectedDate}
-                  setSelectedDate={setSelectedDate}
-                  eventArrayFromAPI={eventArrayFromAPI}
-                />
-              ) : step === 2 ? (
-                <Ou
-                  events={filterByDate(eventArrayFromAPI, selectedDate)}
-                  selectedPlace={selectedPlace}
-                  setSelectedPlace={setSelectedPlace}
-                />
-              ) : (
-                <CardShowResults
-                  events={filterByLocation(
-                    eventArrayFromAPI,
-                    selectedDate,
-                    selectedPlace
-                  )}
-                />
-              )
+              <Parcours
+                selectedDate={selectedDate}
+                setSelectedDate={setSelectedDate}
+                selectedPlace={selectedPlace}
+                setSelectedPlace={setSelectedPlace}
+                eventArrayFromAPI={eventArrayFromAPI}
+              />
             }
           />
           <Route path="/nav" element={<Navbar />} />
@@ -114,11 +87,6 @@ function App() {
           <Route path="/event/:id" element={<Detailspretext />} />
         </Routes>
       </Router>
-      <BtnNav
-        step={step}
-        handleSubmitNext={handleSubmitNext}
-        handleSubmitPrev={handleSubmitPrev}
-      />
     </div>
   );
 }
---
+++ frontend/src/components/parcours.jsx
@@ -0,0 +1,65 @@
+/* eslint-disable no-nested-ternary */
+import React, { useState } from "react";
+import Quand from "../pages/Quand";
+import Accueil from "../pages/Accueil";
+import Ou from "../pages/Ou";
+import CardShowResults from "./CardShowResults";
+import { filterByDate, filterByLocation } from "./functions";
+import BtnNav from "./BtnNav";
+import "../App.css";
+import "../Background.scss";
+
+function Parcours({
+  selectedDate,
+  setSelectedDate,
+  selectedPlace,
+  setSelectedPlace,
+  eventArrayFromAPI,
+}) {
+  const [step, setStep] = useState(0);
+
+  const handleSubmitNext = () => {
+    if (step < 4) {
+      setStep(step + 1);
+    }
+  };
+  const handleSubmitPrev = () => {
+    if (step > 0) {
+      setStep(step - 1);
+    }
+  };
+  return (
+    <div>
+      {step === 0 ? (
+        <Accueil />
+      ) : step === 1 ? (
+        <Quand
+          selectedDate={selectedDate}
+          setSelectedDate={setSelectedDate}
+          eventArrayFromAPI={eventArrayFromAPI}
+        />
+      ) : step === 2 ? (
+        <Ou
+          events={filterByDate(eventArrayFromAPI, selectedDate)}
+          selectedPlace={selectedPlace}
+          setSelectedPlace={setSelectedPlace}
+        />
+      ) : (
+        <CardShowResults
+          events={filterByLocation(
+            eventArrayFromAPI,
+            selectedDate,
+            selectedPlace
+          )}
+        />
+      )}
+      <BtnNav
+        step={step}
+        handleSubmitNext={handleSubmitNext}
+        handleSubmitPrev={handleSubmitPrev}
+      />
+    </div>
+  );
+}
+
+export default Parcours;
