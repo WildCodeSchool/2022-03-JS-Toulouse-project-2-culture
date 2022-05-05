8180ff86abb451f3a98c9aaa03b9e33c887dd022
Xaboule <xaboule@gmail.com>
2022-05-05 16:33:45 +0200

    et la ca push ou bien?

---
+++ frontend/src/components/CardShowResults copy.jsx
@@ -0,0 +1,131 @@
+import React from "react";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
+import CardTheme from "./CardTheme";
+
+import "./carousel.css";
+import "./CardTheme.css";
+import { dateJJMMConverter } from "./functions";
+
+function CardShowResults(props) {
+  const { events } = props;
+
+  const eventsCulture = events.filter(
+    (event) => event.fields.thematique === "Culture"
+  );
+  const eventsEnvironnement = events.filter(
+    (event) => event.fields.thematique === "Environnement"
+  );
+  const eventsSport = events.filter(
+    (event) => event.fields.thematique === "Sport"
+  );
+  const eventsBrocantes = events.filter(
+    (event) =>
+      event.fields.thematique === "Vides Grenier / Brocantes / Foires et salons"
+  );
+  const eventsAutres = events.filter(
+    (event) => event.fields.thematique === "Autres"
+  );
+  return (
+    <div className="showcase">
+      <h2>Vos pretextes</h2>
+      <h3>Intello...</h3>
+      <div className="carousel-container">
+        <Carousel showThumbs={false} showStatus={false}>
+          {eventsCulture.length > 0
+            ? eventsCulture.map((event) => (
+                <div key={event.recordid} className="carousel-card">
+                  <CardTheme
+                    title={event.fields.titre}
+                    date={dateJJMMConverter(event.fields.date_debut)}
+                    isFavorite={false}
+                    stylecard="Culture"
+                    recordid={event.recordid}
+                  />
+                </div>
+              ))
+            : "Pas de résultats"}
+        </Carousel>
+      </div>
+      <h3>Ecolo...</h3>
+      <div className="carousel-container">
+        <Carousel showThumbs={false} showStatus={false}>
+          {eventsEnvironnement.length > 0
+            ? eventsEnvironnement.map((event) => (
+                <div key={event.recordid}>
+                  <CardTheme
+                    key={event.recordid}
+                    title={event.fields.titre}
+                    date={dateJJMMConverter(event.fields.date_debut)}
+                    isFavorite={false}
+                    stylecard="Environnement"
+                    recordid={event.recordid}
+                  />
+                </div>
+              ))
+            : "Pas de résultats"}
+        </Carousel>
+      </div>
+
+      <h3>Sportif...</h3>
+      <div className="carousel-container">
+        <Carousel showThumbs={false} showStatus={false}>
+          {eventsSport.length > 0
+            ? eventsSport.map((event) => (
+                <div key={event.recordid}>
+                  <CardTheme
+                    key={event.recordid}
+                    title={event.fields.titre}
+                    date={dateJJMMConverter(event.fields.date_debut)}
+                    isFavorite={false}
+                    stylecard="Sport"
+                    recordid={event.recordid}
+                  />
+                </div>
+              ))
+            : "Pas de résultats"}
+        </Carousel>
+      </div>
+      <h3>Flanneur...</h3>
+      <div className="carousel-container">
+        <Carousel showThumbs={false} showStatus={false}>
+          {eventsBrocantes.length > 0
+            ? eventsBrocantes.map((event) => (
+                <div key={event.recordid}>
+                  <CardTheme
+                    key={event.recordid}
+                    title={event.fields.titre}
+                    date={dateJJMMConverter(event.fields.date_debut)}
+                    isFavorite={false}
+                    stylecard="Brocantes"
+                    recordid={event.recordid}
+                  />
+                </div>
+              ))
+            : "Pas de résultats"}
+        </Carousel>
+      </div>
+      <h3>Autres...</h3>
+      <div className="carousel-container">
+        <Carousel showThumbs={false} showStatus={false}>
+          {eventsAutres.length > 0
+            ? eventsAutres.map((event) => (
+                <div key={event.recordid}>
+                  <CardTheme
+                    key={event.recordid}
+                    title={event.fields.titre}
+                    date={dateJJMMConverter(event.fields.date_debut)}
+                    isFavorite={false}
+                    stylecard="Autres"
+                    recordid={event.recordid}
+                  />
+                </div>
+              ))
+            : "Pas de résultats"}
+        </Carousel>
+      </div>
+    </div>
+  );
+}
+
+export default CardShowResults;
--- frontend/src/components/CardShowResults.jsx
+++ frontend/src/components/CardShowResults.jsx
@@ -1,11 +1,11 @@
+/* eslint-disable no-unused-vars */
 import React from "react";
-import "react-responsive-carousel/lib/styles/carousel.min.css";
-import { Carousel } from "react-responsive-carousel";
-import CardTheme from "./CardTheme";
-
-import "./carousel.css";
-import "./CardTheme.css";
+import Slider from "react-slick";
+// import Carousel from "react-slick";
 import { dateJJMMConverter } from "./functions";
+import CardTheme from "./CardTheme";
+import "./carousel2.css";
+// import "./CardTheme.css";
 
 function CardShowResults(props) {
   const { events } = props;
@@ -26,12 +26,21 @@ function CardShowResults(props) {
   const eventsAutres = events.filter(
     (event) => event.fields.thematique === "Autres"
   );
+  /* eslint-disable react/jsx-props-no-spreading */
+  // eslint-disable-next-line react/prefer-stateless-function
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
   return (
-    <div className="showcase">
-      <h2>Vos pretextes</h2>
-      <h3>Intello...</h3>
-      <div className="carousel-container">
-        <Carousel showThumbs={false} showStatus={false}>
+    <div className="Carousel">
+      <h2> Vos pretextes</h2>
+      <Slider {...settings}>
+        <div className="carousel-container">
+          <h3>Intello...</h3>
           {eventsCulture.length > 0
             ? eventsCulture.map((event) => (
                 <div key={event.recordid} className="carousel-card">
@@ -45,85 +54,23 @@ function CardShowResults(props) {
                 </div>
               ))
             : "Pas de résultats"}
-        </Carousel>
-      </div>
-      <h3>Ecolo...</h3>
-      <div className="carousel-container">
-        <Carousel showThumbs={false} showStatus={false}>
-          {eventsEnvironnement.length > 0
-            ? eventsEnvironnement.map((event) => (
-                <div key={event.recordid}>
-                  <CardTheme
-                    key={event.recordid}
-                    title={event.fields.titre}
-                    date={dateJJMMConverter(event.fields.date_debut)}
-                    isFavorite={false}
-                    stylecard="Environnement"
-                    recordid={event.recordid}
-                  />
-                </div>
-              ))
-            : "Pas de résultats"}
-        </Carousel>
-      </div>
-
-      <h3>Sportif...</h3>
-      <div className="carousel-container">
-        <Carousel showThumbs={false} showStatus={false}>
-          {eventsSport.length > 0
-            ? eventsSport.map((event) => (
-                <div key={event.recordid}>
-                  <CardTheme
-                    key={event.recordid}
-                    title={event.fields.titre}
-                    date={dateJJMMConverter(event.fields.date_debut)}
-                    isFavorite={false}
-                    stylecard="Sport"
-                    recordid={event.recordid}
-                  />
-                </div>
-              ))
-            : "Pas de résultats"}
-        </Carousel>
-      </div>
-      <h3>Flanneur...</h3>
-      <div className="carousel-container">
-        <Carousel showThumbs={false} showStatus={false}>
-          {eventsBrocantes.length > 0
-            ? eventsBrocantes.map((event) => (
-                <div key={event.recordid}>
-                  <CardTheme
-                    key={event.recordid}
-                    title={event.fields.titre}
-                    date={dateJJMMConverter(event.fields.date_debut)}
-                    isFavorite={false}
-                    stylecard="Brocantes"
-                    recordid={event.recordid}
-                  />
-                </div>
-              ))
-            : "Pas de résultats"}
-        </Carousel>
-      </div>
-      <h3>Autres...</h3>
-      <div className="carousel-container">
-        <Carousel showThumbs={false} showStatus={false}>
-          {eventsAutres.length > 0
-            ? eventsAutres.map((event) => (
-                <div key={event.recordid}>
-                  <CardTheme
-                    key={event.recordid}
-                    title={event.fields.titre}
-                    date={dateJJMMConverter(event.fields.date_debut)}
-                    isFavorite={false}
-                    stylecard="Autres"
-                    recordid={event.recordid}
-                  />
-                </div>
-              ))
-            : "Pas de résultats"}
-        </Carousel>
-      </div>
+        </div>
+        <div>
+          <h3>2</h3>
+        </div>
+        <div>
+          <h3>3</h3>
+        </div>
+        <div>
+          <h3>4</h3>
+        </div>
+        <div>
+          <h3>5</h3>
+        </div>
+        <div>
+          <h3>6</h3>
+        </div>
+      </Slider>
     </div>
   );
 }
